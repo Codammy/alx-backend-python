28cc5bfe34a580d5fa7aa9f5122865d07dffdf52
codam <damisco005@gmail.com>
2024-10-11 11:20:02 +0100

    task 0 and 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/sur/bin/python3
+"""
+    type annotated function that sums up two numbers
+"""
+
+
+def add(a:  float, b: float) -> float:
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    type-annotated function concat that
+    takes a stringstr1 and a string str2 as
+    arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
