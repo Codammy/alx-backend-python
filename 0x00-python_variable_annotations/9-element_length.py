81241292ab77ed4992abfb59e50d03d4d7ec9551
codam <damisco005@gmail.com>
2024-10-13 07:04:59 +0100

    0-main.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,4 +7,5 @@ import typing
 
 def element_length(lst: typing.Iterable[typing.Sequence])\
       -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    """annotated function element_length"""
     return [(i, len(i)) for i in lst]
