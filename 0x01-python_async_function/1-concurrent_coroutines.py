43dadb5492de7c1bbb1e61e1995abe01e8b46b83
codam <damisco005@gmail.com>
2024-10-19 04:52:35 +0100

    README.md

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    an async routine called wait_n that takes in 2 int arguments
+    (in this order): n and max_delay. You will spawn wait_random
+    n times with the specified max_delay.
+"""
+import asyncio
+import typing
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """return the list of all the delays (float values)."""
+    waits = [await wait_random(max_delay) for i in range(n)]
+    custom_sort(waits)
+    return waits
+
+
+def custom_sort(lst: typing.List[float]) -> None:
+    """custom sort function implementing bubble sort alg"""
+    for pos in range(len(lst)):
+        for i in range(len(lst)-1):
+            if lst[i] > lst[i + 1]:
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
