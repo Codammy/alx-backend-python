380cda5255475922d64528cb41925ac26a90cd47
codam <damisco005@gmail.com>
2024-10-19 05:48:57 +0100

    README.md

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-
+    an async routine called wait_n that takes in 2 int arguments
+    (in this order): n and max_delay. You will spawn task_wait_random
+    n times with the specified max_delay.
 """
 import typing
 task_wait_random = __import__("3-tasks").task_wait_random
